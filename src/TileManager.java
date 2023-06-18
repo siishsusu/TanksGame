36eb795b140a70f7917df118dea014f69a993e08
haryok123 <kharyshyn.igor@gmail.com>
2023-06-18 19:24:09 +0300

    дизайн+ карти

--- src/TileManager.java
+++ src/TileManager.java
@@ -11,82 +11,94 @@ public class TileManager {
     GamePanel panel;
     Tile[] tile;
     int mapTileNum[][];
-    public TileManager(GamePanel panel){
-        this.panel=panel;
-        tile=new Tile[10];
-        mapTileNum=new int[panel.maxWorldCol][panel.maxWorldRow];
+
+    public TileManager(GamePanel panel) {
+        this.panel = panel;
+        tile = new Tile[10];
+        mapTileNum = new int[panel.maxWorldCol][panel.maxWorldRow];
         getTileImage();
         loadMap();
     }
+
     public void getTileImage() {
-            tile[0]=new Tile();
-            tile[0].image= new ImageIcon("imgs/grass.png").getImage();
-            tile[0].brake=true;
-            tile[1]=new Tile();
-            tile[1].image= new ImageIcon("imgs/brickWall.jpg").getImage();
-            tile[1].collide=true;
-            tile[2]=new Tile();
-            tile[2].image= new ImageIcon("imgs/water.jpg").getImage();
-            tile[2].livesDown = true;
-            tile[3]=new Tile();
-            tile[3].image= new ImageIcon("imgs/lava.jpg").getImage();
-            tile[3].livesDown = true;
-            tile[4]=new Tile();
-            tile[4].image= new ImageIcon("imgs/tree1.png").getImage();
-            tile[4].collide=true;
-            tile[5]=new Tile();
-            tile[5].image= new ImageIcon("imgs/sand1.png").getImage();
-            tile[6]=new Tile();
-            tile[6].image= new ImageIcon("imgs/tree2.png").getImage();
-            tile[6].collide=true;
-            tile[7]=new Tile();
-            tile[7].image= new ImageIcon("imgs/chest.png").getImage();
-            tile[7].collide=true;
+        tile[0] = new Tile();
+        tile[0].image = new ImageIcon("imgs/grass.png").getImage();
+        tile[0].brake = true;
+        tile[1] = new Tile();
+        tile[1].image = new ImageIcon("imgs/brickwall-5.png").getImage();
+        tile[1].collide = true;
+        tile[2] = new Tile();
+        tile[2].image = new ImageIcon("imgs/water.jpg").getImage();
+        tile[2].livesDown = true;
+        tile[3] = new Tile();
+        tile[3].image = new ImageIcon("imgs/lava.jpg").getImage();
+        tile[3].livesDown = true;
+        tile[4] = new Tile();
+        tile[4].image = new ImageIcon("imgs/tree1.png").getImage();
+        tile[4].collide = true;
+        tile[5] = new Tile();
+        tile[5].image = new ImageIcon("imgs/sand1.png").getImage();
+        tile[6] = new Tile();
+        tile[6].image = new ImageIcon("imgs/tree2.png").getImage();
+        tile[6].collide = true;
+        tile[7] = new Tile();
+        tile[7].image = new ImageIcon("imgs/chest.png").getImage();
+        tile[7].collide = true;
+        tile[8] = new Tile();
+        tile[8].image = new ImageIcon("imgs/tree-sand.png").getImage();
+        tile[8].collide = true;
+        tile[9] = new Tile();
+        tile[9].image = new ImageIcon("imgs/brickWall.jpg").getImage();
+        tile[9].collide = true;
 
     }
-    public void loadMap(){
+
+    public void loadMap() {
         try {
-            File fe = new File("test.txt");
+            File fe = new File("boss-map.txt");
             FileInputStream fis = new FileInputStream(fe);
             BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
 
-            int col=0, row=0;
-            while (col<panel.maxWorldCol && row<panel.maxWorldRow) {
+            int col = 0, row = 0;
+            while (col < panel.maxWorldCol && row < panel.maxWorldRow) {
                 String line = reader.readLine();
-                while (col<panel.maxWorldCol){
+                while (col < panel.maxWorldCol) {
                     String[] textures = line.split(" ");
                     int texture = Integer.parseInt(textures[col]);
-                    mapTileNum[col][row]=texture;
+                    mapTileNum[col][row] = texture;
                     col++;
                 }
                 if (col == panel.maxWorldCol) {
                     col = 0;
                     row++;
                 }
-            }reader.close();
+            }
+            reader.close();
 
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
     }
-    public void paintCell(int row, int col, int tile){
-        int texture = 0;
-        if(tile==0) texture = 5;
-        mapTileNum[col][row]=texture;
-    }
-    public void draw(Graphics2D g2){
-        int colWorld = 0, rowWorld = 0;
-        while (colWorld<panel.maxWorldCol && rowWorld<panel.maxWorldRow) {
-            int tileNum=mapTileNum[colWorld][rowWorld];
-            int worldX = colWorld*panel.tankSize;
-            int worldY = rowWorld*panel.tankSize;
-            int screenX = worldX-panel.player.playerX+panel.player.screenX;
-            int screenY = worldY-panel.player.playerY+panel.player.screenY;
 
-            if(worldX+panel.tankSize>panel.player.playerX-panel.player.screenX &&
-                    worldX-panel.tankSize<panel.player.playerX+panel.player.screenX &&
-                    worldY+panel.tankSize>panel.player.playerY-panel.player.screenY &&
-                    worldY-panel.tankSize<panel.player.playerY+panel.player.screenY){
+    public void paintCell(int row, int col, int tile) {
+        int texture = 0;
+        if (tile == 0) texture = 5;
+        mapTileNum[col][row] = texture;
+    }
+
+    public void draw(Graphics2D g2) {
+        int colWorld = 0, rowWorld = 0;
+        while (colWorld < panel.maxWorldCol && rowWorld < panel.maxWorldRow) {
+            int tileNum = mapTileNum[colWorld][rowWorld];
+            int worldX = colWorld * panel.tankSize;
+            int worldY = rowWorld * panel.tankSize;
+            int screenX = worldX - panel.player.playerX + panel.player.screenX;
+            int screenY = worldY - panel.player.playerY + panel.player.screenY;
+
+            if (worldX + panel.tankSize > panel.player.playerX - panel.player.screenX &&
+                    worldX - panel.tankSize < panel.player.playerX + panel.player.screenX &&
+                    worldY + panel.tankSize > panel.player.playerY - panel.player.screenY &&
+                    worldY - panel.tankSize < panel.player.playerY + panel.player.screenY) {
                 g2.drawImage(tile[tileNum].image, screenX, screenY, panel.tankSize, panel.tankSize, null);
             }
             colWorld++;
